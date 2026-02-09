7c03be7cfcd1ea02db98a214941219e333f8b568
Pietrojoshua <pietrocarlosmunoz12@gmail.com>
2026-02-09 10:37:47 -0300

    refact: declarando explicitamente os tipos

--- models/database.py
+++ models/database.py
@@ -1,33 +1,46 @@
 from sqlite3 import Connection, connect, Cursor
-from typing import Any
+from types import TracebackType
+from typing import Any, Self, Optional, Type
+import traceback
 
 class Database:
     def __init__(self, db_name: str) -> None:
         self.connection: Connection = connect(db_name)
         self.cursor: Cursor = self.connection.cursor()
 
-    def executar(self, query: str, params: tuple = ()) -> Cursor:  
-        self.cursor.execute(query, params)      
+    def executar(self, query: str, params: tuple = ()) -> Cursor:
+        self.cursor.execute(query, params)
         self.connection.commit()
         return self.cursor
     
     def buscar_tudo(self, query: str, params: tuple = ()) -> list[Any]:
         self.cursor.execute(query, params)
-        return self.cursor.fetchall() 
-
+        return self.cursor.fetchall()
+    
     def close(self) -> None:
         self.connection.close()
 
-# Métodos para o gerenciamento de contexto
-
-    def __enter__(self):
-        return self    
     
-    #Método de saída do contexto
-    def __exit__(self, exc_type, exc_value, traceback):
+    # Métodos para o gerenciamento de contexto
+    # Método de entrada no contexto
+    def __enter__(self) -> Self:
+        return self
+    
+    # Método de saída do contexto
+    def __exit__(self, exc_type: Optional[Type[BaseException]], exc_value: Optional[BaseException], tb: Optional[TracebackType]) -> None:
+        
+        if exc_type is not None:
+            print('Exceção capturada no contexto:')
+            print(f'Tipo: {exc_type.__name__}')
+            print(f'Mensagem: {exc_value}')
+            print('Traceback completo:')
+            traceback.print_tb(tb)
+
+
         self.close()
 
 
+# Área de Testes
 # try:
 #     db = Database('./data/tarefas.sqlite3')
 #     db.executar('''
@@ -35,11 +48,11 @@ class Database:
 #         id INTEGER PRIMARY KEY AUTOINCREMENT,
 #         titulo_tarefa TEXT NOT NULL,
 #         data_conclusao TEXT);
-#     ''')        
-#     db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES(?, ?);',
-#     ('Esudar Python', '2026-02-02'))            
+#     ''')
+#     db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Estudar Python', '2026-02-02'))
 # except Exception as e:
-#     print(f"Erro ao cirar a tabela:  {e}")
-#     db.close()
+#     print(f"Erro ao criar a tabela: {e}")
 # finally:
-#     db.close()   
+#     db.close()
+
+
--- teste.py
+++ teste.py
@@ -2,5 +2,7 @@ from models.database import Database
 
 # Teste do gerenciador de contexto
 with Database('./data/tarefas.sqlite3') as db:
-    db.executar('INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', ('Usar o gerenciador de contexto', '2026-02-03'
-    ))
+    db.executar(
+        'INSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);', 
+        ('Usar o gerenciador de contexto', '2026-02-03')
+    )
